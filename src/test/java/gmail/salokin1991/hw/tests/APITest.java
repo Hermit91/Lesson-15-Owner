d93fd378060b15660f88cc6362d4dc7c567f5971
Hermit91 <work.salokin@gmail.com>
2021-12-19 00:14:04 +0300

    addet second test in API fix token value

--- src/test/java/gmail/salokin1991/hw/tests/APITest.java
+++ src/test/java/gmail/salokin1991/hw/tests/APITest.java
@@ -4,6 +4,12 @@ import gmail.salokin1991.hw.config.APIConfig;
 import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class APITest {
@@ -12,6 +18,20 @@ public class APITest {
     public void apiTest() {
         APIConfig apiConfig = ConfigFactory.create(APIConfig.class, System.getProperties());
         assertThat(apiConfig.baseUrl()).isEqualTo("https://github.com");
-        assertThat(apiConfig.token()).isEqualTo("some token");
+        assertThat(apiConfig.token()).isEqualTo("some_token");
+    }
+
+    @Test
+    public void apiWithTempFileTest() throws IOException {
+        String content = "token=some_token";
+        Path props = Paths.get("C:/Windows/Temp/token.properties");
+
+        Files.write(props, content.getBytes(StandardCharsets.UTF_8));
+
+        APIConfig apiConfig = ConfigFactory.create(APIConfig.class, System.getProperties());
+        assertThat(apiConfig.baseUrl()).isEqualTo("https://github.com");
+        assertThat(apiConfig.token()).isEqualTo("some_token");
+
+        Files.delete(props);
     }
 }
